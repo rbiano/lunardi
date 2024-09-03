b8f9d409bee00b9ef069f4f8f4cd86d2d3588b3b
Rafael biano <164901522+rbiano@users.noreply.github.com>
2024-09-03 09:41:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,159 @@
+export const perguntas = [
+    {
+        enunciado: "Qual esporte é conhecido como o esporte das multidões?",
+        alternativas: [
+            "A) Futebol",
+            "B) Tênis",
+            {
+                afirmacao: [
+                    "O futebol é conhecido como o esporte das multidões por ser o mais popular do mundo,",
+                    "atraindo grandes torcidas em estádios e audiências enormes em transmissões.",
+                ],
+                proxima: 1,
+            },
+            {
+            }
+        ]
+    },
+    {
+        enunciado: "Quantos jogadores há em uma equipe de basquete em quadra?",
+        alternativas: [
+            "A) 5 jogadores",
+            "B) 7 jogadores",
+            {
+                afirmacao: [
+                    "No basquete, cada equipe tem 5 jogadores em quadra ao mesmo tempo.",
+                ],
+                proxima: 2,
+            },
+            {
+            }
+                ]
+},
+{
+        enunciado: "Em qual esporte se disputa a Taça Davis?",
+        alternativas: [
+            "A) Tênis",
+            "B) Golfe",
+                        {
+                afirmacao: [
+                    "A Taça Davis é uma competição internacional de tênis",
+                     "entre equipes masculinas.",
+                ],
+                proxima: 3,
+            },
+            {
+            }
+        ]
+    },
+    {
+        enunciado: "Quantos sets são disputados em uma partida de vôlei?",
+        alternativas: [
+            "A) 3 sets",
+            "B) 5 sets",
+            {
+                afirmacao: [
+                    "No vôlei, uma partida é disputada em até 5 sets.",
+                    "A equipe que vencer 3 sets primeiro, ganha a partida.",
+                ],
+                proxima: 4,
+            },
+            {
+            }
+                ]
+            },
+            {
+        enunciado: "Qual é o objetivo principal do jogo de beisebol?",
+        alternativas: [
+            "A) Fazer mais pontos (corridas) que o adversário",
+            "B) Impedir o adversário de tocar na bola",
+            {
+                afirmacao: [
+                    " No beisebol, o objetivo é fazer mais pontos (corridas) que",
+                     "o adversário ao completar voltas pelas bases.",
+                ],
+                proxima: 5,
+            },
+            {
+            }
+                ]
+},
+{
+        enunciado: "O que significa knockout no boxe?",
+        alternativas: [
+            "A) Quando um lutador é nocauteado e não consegue continuar a luta",
+            "B) Quando um lutador ganha por pontos",
+            {
+                afirmacao: [
+                    "Knockout ocorre quando um lutador é nocauteado e não",
+                    "consegue se levantar dentro do tempo permitido, encerrando a luta.",
+                ],
+                proxima: 6,
+            },
+            {
+            }
+                ],
+    },
+    {
+        enunciado: "Quantos jogadores compõem um time de futebol americano em campo?",
+        alternativas: [
+            "A) 11 jogadores",
+            "B) 9 jogadores",
+            {
+                afirmacao: [
+                    "No futebol americano, cada time tem 11 jogadores em campo ao mesmo tempo.",
+                ],
+                proxima: 7,
+            },
+            {
+            },
+        ]
+    },
+    {
+        enunciado: "Em qual esporte se usa a raquete mais leve?",
+        alternativas: [
+            "A) Tênis de mesa",
+            "B) Badminton",
+            {
+                afirmacao: [
+                    "No tênis de mesa, a raquete é mais leve do que no badminton.",
+                ],
+            },
+            {
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a prova mais longa em uma competição de atletismo?",
+        alternativas:[ 
+            "A) Maratona",
+            "B) 100 metros rasos",
+            {
+                afirmacao:[
+                    "A maratona é a prova mais longa em competições de atletismo,",
+                     "com uma distância de 42,195 km.",
+                ],
+                proxima: 8,
+                },
+                {
+                }
+            ]
+        },
+        {
+            enunciado: "Qual esporte é praticado em uma piscina e envolve marcar gols?",
+            alternativas: [
+                "A) Nado sincronizado",
+                "B) Polo aquático",
+                {
+                    afirmacao: [
+                        " O polo aquático é um esporte jogado em uma piscina",
+                        "onde os jogadores tentam marcar gols na baliza do adversário.",
+                    ]
+                    },
+                    {
+                    }
+                ]
+            },
+        {
+    }
+];
